f96dcf443c46c79c9c6ccb58f68ee0a9d8279a20
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-10-06 16:48:17 +0100

    Finished Viewing Hit Points
    Co-Authored-by: Anthony <dajoxx@gmail.com>
    Co-Authored-by: Kostas <kostasntanos@hotmail.com>

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -10,3 +10,14 @@ feature 'Enter names' do
     expect(page).to have_content 'Charlotte vs. Mittens'
   end
 end
+
+feature "View Hit points" do
+  scenario 'see Player 2 Hit points' do
+    visit('/')
+    fill_in :player_1_name, with: 'Charlotte'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+
+    expect(page).to have_content 'Mittens: 60HP'
+  end
+end
