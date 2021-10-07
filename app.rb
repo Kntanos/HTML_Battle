8ed7ca502889df2dc0f04e5c84a8f407727c9f4a
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-10-07 16:01:59 +0100

    Finished Implementing hit points

--- app.rb
+++ app.rb
@@ -13,22 +13,23 @@ class Battle < Sinatra::Base
     erb(:index)
   end
 
-  get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    erb(:play)
-  end
-
   post '/names' do
     $player_1 = Player.new(params[:player_1_name])
     $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
+  get '/play' do
+    @player_1 = $player_1
+    @player_2 = $player_2
+    erb(:play)
+  end
+
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    erb(:attack)
+    @player_1 = $player_1
+    @player_2 = $player_2
+    @player_1.attack(@player_2)
+    erb :attack
   end
 
   run! if app_file == $0
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,8 +1,29 @@
 describe Player do
+  subject(:charlotte) { Player.new('Charlotte') }
+  subject(:mittens) { Player.new('Mittens') }
+
   describe '#player_name' do
     it "should return the player's name" do
-      player_1 = Player.new("Charlotte")
-      expect(player_1.name).to eq("Charlotte")
+      expect(charlotte.name).to eq("Charlotte")
+    end
+  end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(charlotte.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(mittens).to receive(:receive_damage)
+      charlotte.attack(mittens)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { charlotte.receive_damage }.to change { charlotte.hit_points }.by(-10)
     end
   end
 end
