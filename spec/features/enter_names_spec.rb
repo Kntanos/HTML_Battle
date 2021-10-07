8a3617799d5e65937e26d9f3fe3418cdb56b6de9
Jacob Cho <jacobcho@live.com>
2021-10-07 16:44:38 +0100

    skinny controller

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,7 @@
 feature 'Enter names' do
   scenario 'Submitting names' do
     sign_in_and_play
-
     # save_and_open_page
-
     expect(page).to have_content 'Charlotte vs. Mittens'
   end
 end
@@ -11,17 +9,14 @@ end
   feature "View Hit points" do
     scenario 'see Player 2 Hit points' do
       sign_in_and_play
-
       expect(page).to have_content 'Mittens: 60HP'
     end
   end
 
   feature "Attacking" do
-    
     scenario 'attacking Player 2' do
       sign_in_and_play
       click_button 'Attack!'
-  
       expect(page).to have_content 'Charlotte attacks Mittens'
     end
     
